c5bcc2cbf043bdd7f34c3b97b333e87eeca4e108
Tom Hombergs <tom.hombergs@gmail.com>
2014-05-31 01:06:39 +0200

    deleted unused imports

--- server/ikenga-web/src/main/java/me/ikenga/web/base/components/BasePage.java
+++ server/ikenga-web/src/main/java/me/ikenga/web/base/components/BasePage.java
@@ -1,13 +1,7 @@
 package me.ikenga.web.base.components;
 
-import me.ikenga.IkengaWebApplication;
-
-import org.apache.wicket.markup.head.CssHeaderItem;
-import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.request.resource.CssResourceReference;
-import org.apache.wicket.request.resource.PackageResourceReference;
 
 public abstract class BasePage extends WebPage {
 
